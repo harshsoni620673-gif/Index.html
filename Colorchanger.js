6908969936d91438b0485979bdbc5c264d9190ab
harshsoni620673-gif <harshsoni620673@gmail.com>
2025-10-22 16:58:05 +0530

    Delete Colorchanger.js

--- Colorchanger.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-  const colors = ['lightblue', 'lightgreen', 'lightpink', 'lightyellow', 'lavender'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
-}
